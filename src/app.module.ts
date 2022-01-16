232e6c5916b58ffa8ce0f2b43476e0c17e99bbed
codegear <dukeom@gmail.com>
2022-01-16 12:00:42 +0900

    init

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cat } from './cats/entity/cats.entity';
+
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 13306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [Cat],
+      synchronize: true,
+    }),
+    CatsModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
